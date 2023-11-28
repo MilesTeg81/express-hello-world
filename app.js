8627f368772b0cf416ec6ae2e994925df8a9339f
MilesTeg97 <64781653+MilesTeg97@users.noreply.github.com>
2023-11-28 15:58:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+//app.get("/", (req, res) => res.type('html').send(html));
 
+app.get('/', (req, res) => res.send('Hello World!'));
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
